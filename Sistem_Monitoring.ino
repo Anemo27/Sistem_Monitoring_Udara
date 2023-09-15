b6b1a58c55b1c55e988599310fea61e00dd7e446
ANeMo <purnomokadir@gmail.com>
2023-09-15 10:50:54 +0700

    Add skema picture

--- Sistem_Monitoring.ino
+++ Sistem_Monitoring.ino
@@ -27,9 +27,8 @@
 #define DSM_CONCENTRATION_C 520.0
 #define DSM_CONCENTRATION_D 0.62
 #define TS_INTERVAL 20000
-#define CHANNEL_ID_EEPROM_ADDR 0      // Alamat EEPROM untuk menyimpan Channel ID
-#define WRITE_API_KEY_EEPROM_ADDR 10  // Alamat EEPROM untuk menyimpan Write API Key
-
+#define CHANNEL_ID_EEPROM_ADDR 0     // Alamat EEPROM untuk menyimpan Channel ID
+#define WRITE_API_KEY_EEPROM_ADDR 10 // Alamat EEPROM untuk menyimpan Write API Key
 
 // ThingSpeak Settings
 char channelID[21];
@@ -54,7 +53,8 @@ unsigned long dsm_lowPulse = 0;
 unsigned long dsm_previousTime = 0;
 unsigned long ts_previousTime = 0;
 
-void setup() {
+void setup()
+{
   WiFi.mode(WIFI_STA);
   Serial.begin(115200);
   Serial.setDebugOutput(true);
@@ -71,13 +71,16 @@ void setup() {
 
   wm.setConfigPortalBlocking(false);
   wm.setSaveParamsCallback(saveParamCallback);
-  std::vector<const char*> menu = { "wifi", "info", "param", "sep", "restart", "exit" };
+  std::vector<const char *> menu = {"wifi", "info", "param", "sep", "restart", "exit"};
   wm.setMenu(menu);
   wm.setClass("invert");
 
-  if (wm.autoConnect(AP_SSID, AP_PASSWORD)) {
+  if (wm.autoConnect(AP_SSID, AP_PASSWORD))
+  {
     Serial.println("Connected to WiFi");
-  } else {
+  }
+  else
+  {
     Serial.println("Config portal running");
   }
 
@@ -95,7 +98,8 @@ void setup() {
 
   Serial.print("Calibrating sensor, please wait");
   float calcR0 = 0;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     MQ7.update();
     calcR0 += MQ7.calibrate(RATIO_CLEANAIR_MQ7);
     Serial.print(".");
@@ -104,7 +108,8 @@ void setup() {
   MQ7.setR0(calcR0 / 10);
   Serial.println(" done!");
 
-  if (isinf(calcR0) || calcR0 == 0) {
+  if (isinf(calcR0) || calcR0 == 0)
+  {
     Serial.println("Warning: Connection issue with MQ7 sensor. Please check wiring and supply.");
     while (1)
       ;
@@ -118,18 +123,21 @@ void setup() {
   pinMode(TRIGGER_PIN, INPUT);
 }
 
-void saveToEEPROM() {
+void saveToEEPROM()
+{
   EEPROM.put(CHANNEL_ID_EEPROM_ADDR, channelID);
   EEPROM.put(WRITE_API_KEY_EEPROM_ADDR, writeAPIKey);
   EEPROM.commit();
 }
 
-void loadFromEEPROM() {
+void loadFromEEPROM()
+{
   EEPROM.get(CHANNEL_ID_EEPROM_ADDR, channelID);
   EEPROM.get(WRITE_API_KEY_EEPROM_ADDR, writeAPIKey);
 }
 
-void saveParamCallback() {
+void saveParamCallback()
+{
   strcpy(channelID, channel_id_param.getValue());
   strcpy(writeAPIKey, api_key_param.getValue());
 
@@ -137,31 +145,34 @@ void saveParamCallback() {
   saveToEEPROM();
 }
 
-float getParticlemgm3(float r) {
+float getParticlemgm3(float r)
+{
   /*
-      * with data sheet...regression function is
-      *    y=0.1776*x^3-2.24*x^2+ 94.003*x
-      */
+   * with data sheet...regression function is
+   *    y=0.1776*x^3-2.24*x^2+ 94.003*x
+   */
   // https://github.com/R2D2-2019/R2D2-2019/wiki/Is-the-given-formula-for-calculating-the-mg-m3-for-the-dust-sensor-dsm501a-correct%3F
 
   float mgm3 = 0.001915 * pow(r, 2) + 0.09522 * r - 0.04884;
   return mgm3 < 0.0 ? 0.0 : mgm3;
 }
 
-void loop() {
+void loop()
+{
   digitalWrite(LED_PIN, HIGH);
   delay(DELAY_TIME);
 
   int currentWiFiStatus = WiFi.status();
   static int lastWiFiStatus = WL_IDLE_STATUS;
-  if (currentWiFiStatus != lastWiFiStatus) {
+  if (currentWiFiStatus != lastWiFiStatus)
+  {
     lastWiFiStatus = currentWiFiStatus;
-    if (currentWiFiStatus != WL_CONNECTED) {
+    if (currentWiFiStatus != WL_CONNECTED)
+    {
       Serial.println("WiFi disconnected. Reconnecting...");
       wm.startConfigPortal(AP_SSID, AP_PASSWORD);
     }
   }
-
 
   // WiFi Manager
   wm.process();
@@ -169,23 +180,33 @@ void loop() {
   // DSM501a
   dsm_lowPulse += pulseIn(DSM_PIN, LOW);
 
-  if ((millis() - dsm_previousTime) > DSM_INTERVAL) {
+  if ((millis() - dsm_previousTime) > DSM_INTERVAL)
+  {
     Serial.println("============================");
     dsm_previousTime = millis();
     float lowRatio = (dsm_lowPulse * DSM_LOW_RATIO_MULTIPLIER) / DSM_INTERVAL;
-    dsm_consentrate = 1.1 * pow(lowRatio, 3) - 3.8 * pow(lowRatio, 2) + 520 * lowRatio + 0.62;  // using spec sheet curve
+    dsm_consentrate = 1.1 * pow(lowRatio, 3) - 3.8 * pow(lowRatio, 2) + 520 * lowRatio + 0.62; // using spec sheet curve
     dsm_particle = getParticlemgm3(lowRatio);
 
     Serial.print("Air Quality         : ");
-    if (dsm_consentrate < 1000) {
+    if (dsm_consentrate < 1000)
+    {
       Serial.println("Air is Clean");
-    } else if (dsm_consentrate < 10000) {
+    }
+    else if (dsm_consentrate < 10000)
+    {
       Serial.println("Air is Good");
-    } else if (dsm_consentrate < 20000) {
+    }
+    else if (dsm_consentrate < 20000)
+    {
       Serial.println("Air is Moderate");
-    } else if (dsm_consentrate < 50000) {
+    }
+    else if (dsm_consentrate < 50000)
+    {
       Serial.println("Air is Heavy");
-    } else {
+    }
+    else
+    {
       Serial.println("Air is Hazardous");
     }
 
@@ -199,7 +220,6 @@ void loop() {
 
     dsm_lowPulse = 0;
 
-
     // MQ-7
     MQ7.update();
     mq7Value = MQ7.readSensor();
@@ -212,9 +232,12 @@ void loop() {
     tempC = dht.readTemperature();
     tempF = dht.readTemperature(true);
 
-    if (isnan(tempC) || isnan(tempF) || isnan(humi)) {
+    if (isnan(tempC) || isnan(tempF) || isnan(humi))
+    {
       Serial.println("Failed to read from DHT sensor!");
-    } else {
+    }
+    else
+    {
       Serial.print("Humidity            : ");
       Serial.print(humi);
       Serial.println("%");
@@ -229,7 +252,8 @@ void loop() {
   }
 
   // ThingSpeak
-  if ((millis() - ts_previousTime) > TS_INTERVAL) {
+  if ((millis() - ts_previousTime) > TS_INTERVAL)
+  {
     Serial.println("============================");
     ts_previousTime = millis();
     ThingSpeak.setField(1, tempC);
@@ -241,19 +265,24 @@ void loop() {
 
     bool sentSuccessfully = false;
     int attempts = 0;
-    while (!sentSuccessfully && attempts < 3) {
+    while (!sentSuccessfully && attempts < 3)
+    {
       int responseCode = ThingSpeak.writeFields(String(channelID).toInt(), writeAPIKey);
-      if (responseCode == 200) {
+      if (responseCode == 200)
+      {
         sentSuccessfully = true;
         Serial.println("Channel update successful.");
-      } else {
+      }
+      else
+      {
         Serial.println("Problem updating channel. HTTP error code " + String(responseCode));
         attempts++;
         delay(1000);
       }
     }
 
-    if (!sentSuccessfully) {
+    if (!sentSuccessfully)
+    {
       Serial.println("Failed to send data to ThingSpeak after several attempts.");
     }
 
